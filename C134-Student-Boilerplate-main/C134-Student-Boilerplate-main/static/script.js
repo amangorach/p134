4b2f8e62b0a16579d7f9fd8324c8c5f5fa789f63
amangorach <91610050+amangorach@users.noreply.github.com>
2023-02-11 15:57:01 +0530

    Update script.js

--- C134-Student-Boilerplate-main/C134-Student-Boilerplate-main/static/script.js
+++ C134-Student-Boilerplate-main/C134-Student-Boilerplate-main/static/script.js
@@ -3,20 +3,20 @@ $(document).ready(function(){
     console.log('Ready')
 
     //  Fetch the current date and update it in the DOM
-    let display_date = "date:" + date.toLocaleDateString()
+    let display_date = "date:" + date.toLocaleDateString(new Date())
     $("#date").html(display_date)
 
 
 
     //  write an event, when Submit button is clicked
-    $('').click(function(){
+    $('#button').click(function(){
 
         //  get the text value from the textarea using the 'val()' method
         let text_value = $('').val()
 
         //  Convert it to JS object.
         //  Provide a 'key' here and in write the same in app.py file as well to extract data
-        let input_text = {'' : text_value}
+        let input_text = {'#text' : text_value}
         console.log(input_text)
 
         //  ajax request
